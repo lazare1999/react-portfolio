b771023a53aa9495a9691a7de2a8244ff26c869e
lazo <lazarekvirtia@gmail.com>
2023-12-23 19:17:02 +0400

    test

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import App from './App';
+
+describe('App component', () => {
+    test('renders header, routes', () => {
+        // Arrange
+        render(
+            <App />
+        );
+
+    });
+});
