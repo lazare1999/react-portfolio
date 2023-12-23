2eaded4379c989b00a1fedfb5d5cfc8a5351f8e8
lazo <lazarekvirtia@gmail.com>
2023-12-23 12:43:47 +0400

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import expect from "expect";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('renders header, routes, and footer', () => {
+    // Arrange
+    render(
+        <App />
+    );
+
+    expect(screen.getByText('About')).toBeInTheDocument();
+    expect(screen.getByText('Home')).toBeInTheDocument();
+  });
 });
