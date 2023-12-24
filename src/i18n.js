f18e9aece35e3456b0ea4b3596eb8a6e3b6be5a4
lazo <lazarekvirtia@gmail.com>
2023-12-24 21:53:42 +0400

    Disable i18n debug

--- src/i18n.js
+++ src/i18n.js
@@ -19,7 +19,7 @@ i18n
     .init({
         resources, // resources are important to load translations for the languages.
         lng: "en", // It acts as default language. When the site loads, content is shown in this language.
-        debug: true,
+        debug: false,
         fallbackLng: "de", // use de if selected language is not available
         interpolation: {
             escapeValue: false
